044d4fbfb0a85e0e0ee4e5223c1ae7d08c2865fe
RajnishRajHello <rajforphoneuse@gmail.com>
2025-10-05 18:54:52 +0530

    fixed vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-})
+  base: process.env.VITE_BASE_PATH || "/react-vite-deploy",
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          firebase: ["firebase/app", "firebase/auth", "firebase/firestore"],
+        },
+      },
+    },
+  },
+});
